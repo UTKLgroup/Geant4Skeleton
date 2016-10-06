90563a613651d1ccbe6002fff23a4f1d415a58f8
dphan <brianp.dung@gmail.com>
2016-10-06 14:30:41 -0500

    Re-organize the GEOMETRY class.

--- include/LSDetectorConstruction.hh
+++ include/LSDetectorConstruction.hh
@@ -14,13 +14,28 @@ class G4VPhysicalVolume;
 class G4LogicalVolume;
 
 class LSDetectorConstruction : public G4VUserDetectorConstruction {
-  public:
+public:
   LSDetectorConstruction();
   virtual ~LSDetectorConstruction();
-
   virtual G4VPhysicalVolume* Construct();
+  virtual void BuildMaterial();
+
+private:
+  /**
+   * WORLD GEOMETRY
+   */
+  G4Box * worldBox;
+  G4LogicalVolume * worldLog;
+  G4VPhysicalVolume * worldPhy;
+  // Dimensions of world volume
+  G4double world_hx = 1.0 * m;
+  G4double world_hy = 1.0 * m;
+  G4double world_hz = 1.0 * m;
+
+  /**
+   * MATERIAL LISTS
+   */
+  G4Material * Air;
 
 };
-
-
 #endif
--- src/LSDetectorConstruction.cc
+++ src/LSDetectorConstruction.cc
@@ -6,29 +6,25 @@ LSDetectorConstruction::LSDetectorConstruction() : G4VUserDetectorConstruction()
 LSDetectorConstruction::~LSDetectorConstruction() {
 }
 
-
-G4VPhysicalVolume * LSDetectorConstruction::Construct() {
+void LSDetectorConstruction::BuildMaterial() {
   G4NistManager * nist = G4NistManager::Instance();
 
-  // check overlapping
+  /**
+   * BASIC MATERIAL
+   */
+  Air = nist->FindOrBuildMaterial("G4_AIR");
+
+}
+
+G4VPhysicalVolume * LSDetectorConstruction::Construct() {
+
+  // Check overlapping
   G4bool checkOverlaps = true;
 
-  // dimensions of world volume
-  G4double world_hx = 1.0 * m;
-  G4double world_hy = 1.0 * m;
-  G4double world_hz = 1.0 * m;
-
-  // material for World volume
-  G4Material * world_mat = nist->FindOrBuildMaterial("G4_AIR");
-
   // create world volume
-  G4Box * worldBox = new G4Box("World", world_hx, world_hy, world_hz);
+  worldBox = new G4Box("World", world_hx, world_hy, world_hz);
+  worldLog = new G4LogicalVolume(worldBox, Air, "World");
+  worldPhy = new G4PVPlacement(0, G4ThreeVector(), worldLog, "World", 0, false, 0, checkOverlaps);
 
-  // create world logical volume
-  G4LogicalVolume * worldLog = new G4LogicalVolume(worldBox, world_mat, "World");
-
-  // create world physical volume centered at origin and unrotated
-  G4VPhysicalVolume * physWorld = new G4PVPlacement(0, G4ThreeVector(), worldLog, "World", 0, false, 0, checkOverlaps);
-
-  return physWorld;
+  return worldPhy;
 }
